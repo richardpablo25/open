f4f73f95a6cd15b02237d42e8932252bcf9c1305
richardpablo25 <richardpablo@hotmail.com>
2022-07-28 15:45:20 -0300

    clase 3

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int resultado = sumar3( 10, 3,4 );
+        System.out.println(resultado);
+
+    }
+    public static int sumar3( int a, int b, int c){
+        return a + b + c;
+    }
+}
